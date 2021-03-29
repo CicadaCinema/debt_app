cc93d77e8166ec83013b075561b845d8a94decca
ColonisationCaptain <52425971+ColonisationCaptain@users.noreply.github.com>
2021-03-29 16:18:04 +0100

    now the sender only has to press a button streambuilder ensures that any transaction request is seen

--- lib/transaction_dispatch.dart
+++ lib/transaction_dispatch.dart
@@ -14,56 +14,35 @@ class _DispatchScreenState extends State<DispatchScreen> {
   CollectionReference users = FirebaseFirestore.instance.collection('users');
   final _formKey = GlobalKey<FormState>();
 
-  String _recipient;
-  double _amount;
+  //String _recipient;
+  //double _amount;
 
-  void dispatchRequest(myContext) {
+  // performs transaction of a specific amount to specific user
+  void dispatchRequest(myContext, recipient, amount) {
     User userCredential = FirebaseAuth.instance.currentUser;
-    String myDisplayName = userCredential.displayName;
+    //String myDisplayName = userCredential.displayName;
     Map<String, dynamic> myDebts = Map<String, dynamic>.from(UserDocStore.userDoc['debts']);
-    Future<QuerySnapshot> recipientFuture = users
-        .where('username', isEqualTo: _recipient)
-        .get();
 
-    recipientFuture.then((QuerySnapshot value) {
-      // various errors are shown as dialog boxes
-      String errorMessage;
-      if (value.size != 1) {
-        errorMessage = 'Found ${value.size} users with this username.';
-      } else if (value.docs.first.data()['pending'] != true) {
-        errorMessage = 'Receiver is not pending';
-      } else if (value.docs.first.data()['pending_user'] != myDisplayName) {
-        errorMessage = 'Username does not match';
-      } else if (value.docs.first.data()['pending_amount'] != _amount) {
-        errorMessage = 'Amount does not match';
-      } else {
-        // attempt to perform transaction
+    // attempt to perform transaction
+    // update debts map
+    myDebts.update(
+        recipient,
+            (var value) => value - amount,
+        ifAbsent: () => -1 * amount);
 
-        // update debts map
-        myDebts.update(
-          _recipient,
-          (var value) => value - _amount,
-          ifAbsent: () => -1 * _amount);
-
-        users.doc(userCredential.uid)
-            .update({
-          'balance': BalanceStore.balance - _amount,
-          'debts': myDebts,
-        })
-            .then((value) {
-          final snackBar = SnackBar(content: Text('Transaction successful'));
-          ScaffoldMessenger.of(context).showSnackBar(snackBar);
-        })
-            .catchError((error) {
-          showDialogBox('Error updating user doc', "Err04: " + error.toString(), myContext);
-        });
-        return;
-      }
-      showDialogBox('Error processing request', errorMessage, myContext);
+    users.doc(userCredential.uid)
+        .update({
+      'balance': BalanceStore.balance - amount,
+      'debts': myDebts,
+    })
+        .then((value) {
+      final snackBar = SnackBar(content: Text('Transaction successful'));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
     })
         .catchError((error) {
-      showDialogBox('Cloud Firestore error', 'Error retrieving data: ' + error.toString(), myContext);
+      showDialogBox('Error updating user doc', "Err04: " + error.toString(), myContext);
     });
+    return;
   }
 
   @override
@@ -75,12 +54,13 @@ class _DispatchScreenState extends State<DispatchScreen> {
           body: Builder(
             // create an inner BuildContext so that the onPressed methods
             // can refer to the Scaffold with Scaffold.of()
+            // TODO: padding is gone after refactoring to ScaffoldMessenger (also gone in transaction_request.dart) - fix this
             builder: (BuildContext innerContext) {
-              return Container(
+              return Center(
                 child: Column(
                   children: <Widget>[
                     Spacer(),
-                    Padding(
+                    /*Padding(
                       padding: EdgeInsets.symmetric(vertical: 10, horizontal: 0),
                       child: TextFormField(
                         validator: (value) {
@@ -121,6 +101,40 @@ class _DispatchScreenState extends State<DispatchScreen> {
                         if (_formKey.currentState.validate()) {
                           dispatchRequest(innerContext);
                         }
+                      },
+                    ),*/
+                    StreamBuilder<QuerySnapshot>(
+                      stream: users.where('pending_user', isEqualTo: FirebaseAuth.instance.currentUser.displayName).snapshots(),
+                      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                        String textMessage;
+
+                        if (snapshot.connectionState == ConnectionState.waiting) {
+                          textMessage = "Loading...";
+                        } else if (snapshot.hasData) {
+                          if (snapshot.data.docs.length == 0) {
+                            textMessage = "Waiting for requests from other users...";
+                          } else if (snapshot.data.docs.length == 1){
+                            // YES - there is exactly one request targeted at the user right now
+                            // this is the desired state for a transaction
+                            Map<String, dynamic> documentData = snapshot.data.docs[0].data();
+                            return ElevatedButton(
+                                onPressed: () {
+                                  dispatchRequest(innerContext, documentData["username"], documentData["pending_amount"]);
+                                },
+                                child: Text('Send £${documentData["pending_amount"].toStringAsFixed(2)} to ${documentData["username"]}'),
+                            );
+                          } else {
+                            textMessage = "Err05: Multiple requests in progress.";
+                          }
+                        } else if (snapshot.hasError) {
+                          textMessage = "Err06: " + snapshot.error.toString();
+                        }
+
+                        else {
+                          textMessage = "Err07: Please report this error.";
+                        }
+
+                        return Text(textMessage);
                       },
                     ),
                     Spacer()
